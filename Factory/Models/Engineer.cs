808ffdb3441860acfb7a4bbddd21944962a0f50b
Drew Henderson <drewhendersonemail@gmail.com>
2022-03-18 13:44:59 -0700

    add models

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int EngineerId { get; set; }
+    public string Name { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime LiscenseRenewal { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
+  }
+}
